4718f1d487ddbc6a5c715bbde8c7ec925f026732
jgaffuri <jgaf@proton.me>
2025-12-08 08:12:32 +0100

    a

--- src/process.py
+++ src/process.py
@@ -9,7 +9,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")
 from geotiff import resample_geotiff_aligned, mask_pixels_with_lambda
 
 #TODO
-# apply mask_pixels_with_lambda to tiffs
 # copy mask_pixels_with_lambda to pysco
 # tile
 # test
@@ -17,9 +16,9 @@ from geotiff import resample_geotiff_aligned, mask_pixels_with_lambda
 # use !
 
 
-aggregate = False
-set_no_data = True
-tiling = False
+aggregate = True
+#set_no_data = True
+tiling = True
 
 input_dem = "/home/juju/geodata/gisco/EU_DEM_mosaic_1000K/eudem_dem_3035_europe.tif"
 resolutions = [1000]  #[ 20000, 10000, 5000, 2000, 1000, 500, 200, 100 ] #, 50, 25
@@ -40,6 +39,7 @@ if aggregate:
             continue
         resample_geotiff_aligned(input_dem, output_dem, resolution, Resampling.med)
 
+'''
 if set_no_data:
     print(datetime.now(), "set no_data")
     for resolution in resolutions:
@@ -47,7 +47,7 @@ if set_no_data:
         input_dem = out_folder+"dem_"+str(resolution) + "m.tif"
         output_dem = out_folder+"dem_"+str(resolution) + "m_nodata.tif"
         mask_pixels_with_lambda(input_dem, output_dem, 1, lambda x: x <= 0)
-
+'''
 
 if tiling:
     print(datetime.now(), "tiling")
@@ -63,8 +63,9 @@ if tiling:
         if not os.path.exists(folder_): os.makedirs(folder_)
 
         # prepare dict for geotiff bands
-        dict = {}
-        dict["v"] = {"file":out_folder+"dem_"+str(resolution) + "m_nodata.tif", "band":1, "no_data_values": [0, None, -9999]}
+        dict = {
+            'v': {"file":out_folder+"dem_"+str(resolution) + "m.tif", "band":1 } #, "no_data_values": [0, None, -9999]}
+        }
 
         # launch tiling
         gridtiler_raster.tiling_raster(
